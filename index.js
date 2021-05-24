0f1b4d4e5e39e5a67c43af8c0b165ac836d8338c
Idifixsmurfen | Walter Te19 <62641006+Idifixsmurfen@users.noreply.github.com>
2021-05-24 14:58:34 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -4,22 +4,89 @@ const client = new Discord.Client()
 
 const prefix1 = 'TUC '
 const prefix2 = 'Count '
-console.log(Date(2))
+let C 
+let MTL
+let HTL
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
 
 client.on('message', msg => {
-  if (msg.content === 'UTC'){
+  if (msg.content.toUpperCase() === 'UTC'){
     msg.reply('Current time is ' + Date())
   }
 
 })
+console.log(parseInt(new Date().getHours()))
+
+client.on('message', msg => {
+  if (msg.content.toUpperCase() === '?LEET?'){
+
+
+  let D = new Date()
+let H = D.getHours()
+let M = D.getMinutes()
+let S = D.getSeconds()
+
+if (H == 12 && M < 37){ MTL = 37 - M;
+console.log(MTL)
+msg.channel.send('time to leet is ' + MTL + ' minutes')
+  // if (C != undefined) { C.send(' until LEETing')}
+}
+
+if (H == 12 && M == 37){ 
+console.log(MTL)
+msg.channel.send('time to leet is 24 hours')
+  // if (C != undefined) { C.send(' until LEETing')}
+}
+
+if (H == 12 && M > 37){ MTL = 60 - M + 37;
+console.log(MTL)
+msg.channel.send('time to leet is 23 hours and ' + MTL + ' minutes')
+  // if (C != undefined) { C.send(' until LEETing')}
+}
+
+  }})
+
+
+
+
+
+setInterval(function() {
+  let D = new Date()
+let H = D.getHours()
+let M = D.getMinutes()
+let S = D.getSeconds()
+
+if (H == 11 && M == 37 && S == 1){
+  if (C != undefined) { C.send('LEET!')}
+}
+},997)
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.on('message', msg => {
+  if (msg.content.toUpperCase() === '?HERE') {
+    C = msg.channel
+
+    msg.channel.send('gotcha fam')
+  }
+})
 
 client.on("message", msg => {
-  if (msg.content === '?ping') {
-    msg.reply('Pong!');
+  if (msg.content.toUpperCase() === '?ping') {
+    msg.reply('Pong!');8
     console.log(msg.content.toUpperCase());
   }
 })
@@ -28,6 +95,7 @@ client.on('message', msg =>{
     msg.reply(msg.content.toUpperCase())
 })
 
+
 client.on('message', msg =>{
     if(!msg.content.startsWith(prefix2) || msg.author.bot) return;
     msg.reply('The character count is: ' + (msg.content.length-6))
@@ -35,7 +103,7 @@ client.on('message', msg =>{
 
 client.on('message', msg => {
   if (msg.content === '?help') {
-    msg.reply('```Type >?ping< for pong! \nType >TUC < for upper case \nType >Count < for character count \n Type >UTC< for time in UTC```')
+    msg.reply('```Type >?ping< for pong! \nType >TUC < for upper case \nType >Count < for character count \nType >UTC< for time in UTC\nType >?here< for LEETing\nType >?leet?< for time until leeting (work in progress)```')
   }
 })
 
@@ -45,4 +113,4 @@ client.on('message', msg => {
   }
 })
 
-client.login('ODQ2MDE3NTQxNjYyMTc5MzY5.YKpZPA.niATxdT7GZblcLLDJB60yMvGGXE')
+client.login('ODQ2MDE3NTQxNjYyMTc5MzY5.YKpZPA.dpD-mRipahsTbAoLP3Iurs2WB2g')
